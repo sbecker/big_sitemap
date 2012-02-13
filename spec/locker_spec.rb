7aef3d1d06ce51f5148e7f886a2c6b0fceb55663
Tobias Bielohlawek <tobi@soundcloud.com>
2012-02-13 10:42:49 +0100

    moved locker into MassiveSitemap scope, cleaned up

---
+++ spec/locker_spec.rb
@@ -0,0 +1,46 @@
+require "massive_sitemap/locker"
+
+describe MassiveSitemap do
+  describe "lock!" do
+    let(:lock_file) { MassiveSitemap::LOCK_FILE }
+
+    after do
+      FileUtils.rm(lock_file) rescue nil
+    end
+
+    it 'does nothing without block' do
+      MassiveSitemap.lock!
+      ::File.exists?(lock_file).should be_false
+    end
+
+    it 'creates lockfile' do
+      File.exists?(lock_file).should be_false
+      MassiveSitemap.lock! do
+        ::File.exists?(lock_file).should be_true
+      end
+    end
+
+    it 'deletes lockfile' do
+      MassiveSitemap.lock! {}
+      ::File.exists?(lock_file).should be_false
+    end
+
+    it 'deletes lockfile in case of error' do
+      expect do
+        MassiveSitemap.lock! do
+          raise ArgumentError
+        end
+      end.to raise_error
+      ::File.exists?(lock_file).should be_false
+    end
+
+    it 'fails if lockfile exists' do
+      ::File.open(lock_file, 'w',) {}
+      expect do
+        MassiveSitemap.lock! do
+          puts "Hi"
+        end
+      end.to raise_error
+    end
+  end
+end
